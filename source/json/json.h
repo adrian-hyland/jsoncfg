3969012ba05a3167d061353e9c10b52671f289df
Adrian Hyland <60901736+adrian-hyland@users.noreply.github.com>
2020-03-28 15:42:26 +0000

    Corrected the quotes used in one of the comments

--- source/json/json.h
+++ source/json/json.h
@@ -36,7 +36,7 @@ int JsonReadFile(tJsonElement *Root, int StripComments, FILE *Stream);
  * @param Stream        The file to wite the JSON content to
  * @return A non-zero (true) value is returned if the JSON content was successfully written to the file
  * @return A zero (false) value is returned if the JSON content could not be written to the file
- * @note If \a 'IndentSize' is zero then the content will use a 'spaced' format. Any comments will also be stripped (the value of \a 'StripComments' will be ignored)
+ * @note If the parameter \a `IndentSize` is zero then the content will use a 'spaced' format. Any comments will also be stripped (the value of the parameter \a `StripComments` will be ignored)
  */
 int JsonWriteFile(tJsonElement *Root, size_t IndentSize, int StripComments, FILE *Stream);
 
