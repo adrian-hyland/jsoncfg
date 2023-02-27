a617826ab70caac0cec67a4297b80ca0449bfe08
Adrian Hyland <60901736+adrian-hyland@users.noreply.github.com>
2023-02-27 11:23:21 +0000

    Using tJsonUtf8Unit for UTF-8 encoded content.

--- source/test/test_json_parse.c
+++ source/test/test_json_parse.c
@@ -4,7 +4,7 @@
 #include "test_json.h"
 
 
-static bool TestJsonParseComplete(const uint8_t *Content, bool StripComments)
+static bool TestJsonParseComplete(const tJsonUtf8Unit *Content, bool StripComments)
 {
 	tJsonElement Root;
 	tJsonParse Parse;
@@ -27,7 +27,7 @@ static bool TestJsonParseComplete(const uint8_t *Content, bool StripComments)
 }
 
 
-static bool TestJsonParseIncomplete(const uint8_t *Content, bool StripComments)
+static bool TestJsonParseIncomplete(const tJsonUtf8Unit *Content, bool StripComments)
 {
 	tJsonElement Root;
 	tJsonParse Parse;
@@ -60,7 +60,7 @@ static bool TestJsonParseIncomplete(const uint8_t *Content, bool StripComments)
 }
 
 
-static bool TestJsonParseError(const uint8_t *Content)
+static bool TestJsonParseError(const tJsonUtf8Unit *Content)
 {
 	tJsonElement Root;
 	tJsonParse Parse;
@@ -85,136 +85,136 @@ static bool TestJsonParseError(const uint8_t *Content)
 
 static bool TestJsonParseContent(void)
 {
-	static const uint8_t *ValidContent[] =
+	static const tJsonUtf8Unit *ValidContent[] =
 	{
-		(const uint8_t *)"{ "
-		                    "\"key-true\": true, "
-		                    "\"key-false\": false, "
-		                    "\"key-null\": null, "
-		                    "\"key-int\": 123, "
-		                    "\"key-real\": 1.23e4, "
-		                    "\"key-real-nan\": NaN, "
-		                    "\"key-string\": \"hello world\", "
-		                    "\"key-string-escape\": \"\\t\\r\\n\\b\\f\\\\\\\"\", "
-		                    "\"key-object-empty\": {}, "
-		                    "\"key-object\": { \"key\": value }, "
-		                    "\"key-array-empty\": [], "
-		                    "\"key-array\": [ "
-		                        "true, "
-		                        "false, "
-		                        "null, "
-		                        "123, "
-		                        "1.23e4, "
-		                        "NaN, "
-		                        "\"hello world\", "
-		                        "\"\\t\\r\\n\\b\\f\\\\\\\"\", "
-		                        "{}, "
-		                        "{ \"key\": value }, "
-		                        "[], "
-		                        "[ 1, 2, 3 ]"
-		                   " ]"
-		                " }",
-		(const uint8_t *)"[ "
-		                    "true, "
-		                    "false, "
-		                    "null, "
-		                    "123, "
-		                    "1.23e4, "
-		                    "NaN, "
-		                    "\"hello world\", "
-		                    "\"\\t\\r\\n\\b\\f\\\\\\\"\", "
-		                    "{}, "
-		                    "{ \"key\": value }, "
-		                    "[], "
-		                    "[ 1, 2, 3 ]"
-		                " ]",
-		(const uint8_t *)"\"string:{}[]\\t\\r\\n\\b\\f\\\\\\\"\"",
-		(const uint8_t *)"true",
-		(const uint8_t *)"false",
-		(const uint8_t *)"null",
-		(const uint8_t *)"1234567890",
-		(const uint8_t *)"1.234567890e-99",
-		(const uint8_t *)"// Comment 1\n"
-		                 "// Comment 2\n"
-		                 "{ // Comment 3\n"
-		                 "  // Comment 4\n"
-		                 "   \"key1\" // Comment 5\n"
-		                 "            // Comment 6\n"
-		                 "   : // Comment 7\n"
-		                 "     // Comment 8\n"
-		                 "   \"value1\" // Comment 9\n"
-		                 "              // Comment 10\n"
-		                 "   , // Comment 11\n"
-		                 "     // Comment 12\n"
-		                 "   \"key2\": [ // Comment 13\n"
-		                 "               // Comment 14\n"
-		                 "      \"value2\" // Comment 15\n"
-		                 "                 // Comment 16\n"
-		                 "      , // Comment 17\n"
-		                 "        // Comment 18\n"
-		                 "      \"value3\" // Comment 19\n"
-		                 "                 // Comment 20\n"
-		                 "   ] // Comment 21\n"
-		                 "     // Comment 22\n"
-		                 "   , // Comment 23\n"
-		                 "     // Comment 24\n"
-		                 "   \"key3\": { // Comment 25\n"
-		                 "               // Comment 26\n"
-		                 "      \"key4\": \"value4\", // Comment 27\n"
-		                 "                            // Comment 28\n"
-		                 "      \"key5\": [] // Comment 29\n"
-		                 "                   // Comment 30\n"
-		                 "   } // Comment 31\n"
-		                 "     // Comment 32\n"
-		                 "} // Comment 33\n"
-		                 "// Comment 34\n"
+		(const tJsonUtf8Unit *)"{ "
+		                          "\"key-true\": true, "
+		                          "\"key-false\": false, "
+		                          "\"key-null\": null, "
+		                          "\"key-int\": 123, "
+		                          "\"key-real\": 1.23e4, "
+		                          "\"key-real-nan\": NaN, "
+		                          "\"key-string\": \"hello world\", "
+		                          "\"key-string-escape\": \"\\t\\r\\n\\b\\f\\\\\\\"\", "
+		                          "\"key-object-empty\": {}, "
+		                          "\"key-object\": { \"key\": value }, "
+		                          "\"key-array-empty\": [], "
+		                          "\"key-array\": [ "
+		                              "true, "
+		                              "false, "
+		                              "null, "
+		                              "123, "
+		                              "1.23e4, "
+		                              "NaN, "
+		                              "\"hello world\", "
+		                              "\"\\t\\r\\n\\b\\f\\\\\\\"\", "
+		                              "{}, "
+		                              "{ \"key\": value }, "
+		                              "[], "
+		                              "[ 1, 2, 3 ]"
+		                         " ]"
+		                      " }",
+		(const tJsonUtf8Unit *)"[ "
+		                          "true, "
+		                          "false, "
+		                          "null, "
+		                          "123, "
+		                          "1.23e4, "
+		                          "NaN, "
+		                          "\"hello world\", "
+		                          "\"\\t\\r\\n\\b\\f\\\\\\\"\", "
+		                          "{}, "
+		                          "{ \"key\": value }, "
+		                          "[], "
+		                          "[ 1, 2, 3 ]"
+		                      " ]",
+		(const tJsonUtf8Unit *)"\"string:{}[]\\t\\r\\n\\b\\f\\\\\\\"\"",
+		(const tJsonUtf8Unit *)"true",
+		(const tJsonUtf8Unit *)"false",
+		(const tJsonUtf8Unit *)"null",
+		(const tJsonUtf8Unit *)"1234567890",
+		(const tJsonUtf8Unit *)"1.234567890e-99",
+		(const tJsonUtf8Unit *)"// Comment 1\n"
+		                       "// Comment 2\n"
+		                       "{ // Comment 3\n"
+		                       "  // Comment 4\n"
+		                       "   \"key1\" // Comment 5\n"
+		                       "            // Comment 6\n"
+		                       "   : // Comment 7\n"
+		                       "     // Comment 8\n"
+		                       "   \"value1\" // Comment 9\n"
+		                       "              // Comment 10\n"
+		                       "   , // Comment 11\n"
+		                       "     // Comment 12\n"
+		                       "   \"key2\": [ // Comment 13\n"
+		                       "               // Comment 14\n"
+		                       "      \"value2\" // Comment 15\n"
+		                       "                 // Comment 16\n"
+		                       "      , // Comment 17\n"
+		                       "        // Comment 18\n"
+		                       "      \"value3\" // Comment 19\n"
+		                       "                 // Comment 20\n"
+		                       "   ] // Comment 21\n"
+		                       "     // Comment 22\n"
+		                       "   , // Comment 23\n"
+		                       "     // Comment 24\n"
+		                       "   \"key3\": { // Comment 25\n"
+		                       "               // Comment 26\n"
+		                       "      \"key4\": \"value4\", // Comment 27\n"
+		                       "                            // Comment 28\n"
+		                       "      \"key5\": [] // Comment 29\n"
+		                       "                   // Comment 30\n"
+		                       "   } // Comment 31\n"
+		                       "     // Comment 32\n"
+		                       "} // Comment 33\n"
+		                       "// Comment 34\n"
 	};
-	static const uint8_t *InvalidContent[] =
+	static const tJsonUtf8Unit *InvalidContent[] =
 	{
-		(const uint8_t *)"{ ,",
-		(const uint8_t *)"{ 1",
-		(const uint8_t *)"{ {",
-		(const uint8_t *)"{ [",
-		(const uint8_t *)"{ :",
-		(const uint8_t *)"{ \"abc\" ,",
-		(const uint8_t *)"{ \"abc\" 1",
-		(const uint8_t *)"{ \"abc\" {",
-		(const uint8_t *)"{ \"abc\" [",
-		(const uint8_t *)"{ \"abc\" \"",
-		(const uint8_t *)"[ ,",
-		(const uint8_t *)"[ :",
-		(const uint8_t *)"[ \"abc\" {",
-		(const uint8_t *)"[ \"abc\" [",
-		(const uint8_t *)"[ \"abc\" :",
-		(const uint8_t *)"[ \"abc\" 1",
-		(const uint8_t *)"[ \"abc\" \"",
-		(const uint8_t *)"[ 123 {",
-		(const uint8_t *)"[ 123 [",
-		(const uint8_t *)"[ 123 :",
-		(const uint8_t *)"[ 123 1",
-		(const uint8_t *)"[ 123 \"",
-		(const uint8_t *)"[ {} {",
-		(const uint8_t *)"[ {} [",
-		(const uint8_t *)"[ {} :",
-		(const uint8_t *)"[ {} 1",
-		(const uint8_t *)"[ {} \"",
-		(const uint8_t *)"[ [] {",
-		(const uint8_t *)"[ [] [",
-		(const uint8_t *)"[ [] :",
-		(const uint8_t *)"[ [] 1",
-		(const uint8_t *)"[ [] \"",
-		(const uint8_t *)"\"abc\" 1",
-		(const uint8_t *)"\"abc\" {",
-		(const uint8_t *)"\"abc\" [",
-		(const uint8_t *)"\"abc\" :",
-		(const uint8_t *)"\"abc\" \"",
-		(const uint8_t *)"\"abc\" ,",
-		(const uint8_t *)"123 1",
-		(const uint8_t *)"123 {",
-		(const uint8_t *)"123 [",
-		(const uint8_t *)"123 :",
-		(const uint8_t *)"123 \"",
-		(const uint8_t *)"123 ,",
+		(const tJsonUtf8Unit *)"{ ,",
+		(const tJsonUtf8Unit *)"{ 1",
+		(const tJsonUtf8Unit *)"{ {",
+		(const tJsonUtf8Unit *)"{ [",
+		(const tJsonUtf8Unit *)"{ :",
+		(const tJsonUtf8Unit *)"{ \"abc\" ,",
+		(const tJsonUtf8Unit *)"{ \"abc\" 1",
+		(const tJsonUtf8Unit *)"{ \"abc\" {",
+		(const tJsonUtf8Unit *)"{ \"abc\" [",
+		(const tJsonUtf8Unit *)"{ \"abc\" \"",
+		(const tJsonUtf8Unit *)"[ ,",
+		(const tJsonUtf8Unit *)"[ :",
+		(const tJsonUtf8Unit *)"[ \"abc\" {",
+		(const tJsonUtf8Unit *)"[ \"abc\" [",
+		(const tJsonUtf8Unit *)"[ \"abc\" :",
+		(const tJsonUtf8Unit *)"[ \"abc\" 1",
+		(const tJsonUtf8Unit *)"[ \"abc\" \"",
+		(const tJsonUtf8Unit *)"[ 123 {",
+		(const tJsonUtf8Unit *)"[ 123 [",
+		(const tJsonUtf8Unit *)"[ 123 :",
+		(const tJsonUtf8Unit *)"[ 123 1",
+		(const tJsonUtf8Unit *)"[ 123 \"",
+		(const tJsonUtf8Unit *)"[ {} {",
+		(const tJsonUtf8Unit *)"[ {} [",
+		(const tJsonUtf8Unit *)"[ {} :",
+		(const tJsonUtf8Unit *)"[ {} 1",
+		(const tJsonUtf8Unit *)"[ {} \"",
+		(const tJsonUtf8Unit *)"[ [] {",
+		(const tJsonUtf8Unit *)"[ [] [",
+		(const tJsonUtf8Unit *)"[ [] :",
+		(const tJsonUtf8Unit *)"[ [] 1",
+		(const tJsonUtf8Unit *)"[ [] \"",
+		(const tJsonUtf8Unit *)"\"abc\" 1",
+		(const tJsonUtf8Unit *)"\"abc\" {",
+		(const tJsonUtf8Unit *)"\"abc\" [",
+		(const tJsonUtf8Unit *)"\"abc\" :",
+		(const tJsonUtf8Unit *)"\"abc\" \"",
+		(const tJsonUtf8Unit *)"\"abc\" ,",
+		(const tJsonUtf8Unit *)"123 1",
+		(const tJsonUtf8Unit *)"123 {",
+		(const tJsonUtf8Unit *)"123 [",
+		(const tJsonUtf8Unit *)"123 :",
+		(const tJsonUtf8Unit *)"123 \"",
+		(const tJsonUtf8Unit *)"123 ,",
 	};
 	size_t n;
 	bool ok;
