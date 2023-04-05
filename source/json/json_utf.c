8af8ed8e103037ea3a5718a89b4b8cd1aebc1fc6
Adrian Hyland <60901736+adrian-hyland@users.noreply.github.com>
2023-04-05 16:03:34 +0100

    Initial check in

---
+++ source/json/json_utf.c
@@ -0,0 +1,88 @@
+#include "json_utf8.h"
+#include "json_utf16.h"
+#include "json_utf.h"
+
+
+size_t JsonUtfGetType(const uint8_t *Content, size_t Length, tJsonUtfType *UtfType)
+{
+	if (Length >= 2)
+	{
+		if ((Content[0] == 0xFE) && (Content[1] == 0xFF))
+		{
+			*UtfType = json_Utf16be;
+			return 2;
+		}
+		if ((Content[0] == 0xFF) && (Content[1] == 0xFE))
+		{
+			*UtfType = json_Utf16le;
+			return 2;
+		}
+		if (Content[0] == 0x00)
+		{
+			if ((Content[1] != 0x00))
+			{
+				*UtfType = json_Utf16be;
+				return 0;
+			}
+		}
+		else if (Content[1] == 0x00)
+		{
+			*UtfType = json_Utf16le;
+			return 0;
+		}
+	}
+	if ((Length >= 3) && (Content[0] == 0xEF) && (Content[1] == 0xBB) && (Content[2] == 0xBF))
+	{
+		*UtfType = json_Utf8;
+		return 3;
+	}
+
+	*UtfType = (Length > 0) ? json_Utf8 : json_UtfUnknown;
+	return 0;
+}
+
+
+size_t JsonUtfDecode(tJsonUtfType UtfType, const uint8_t *Content, size_t Length, size_t Offset, tJsonCharacter *Character)
+{
+	switch (UtfType)
+	{
+		case json_Utf8:
+			return JsonUtf8DecodeNext(Content, Length, Offset, Character);
+		break;
+
+		case json_Utf16be:
+			return JsonUtf16beDecodeNext(Content, Length, Offset, Character);
+		break;
+
+		case json_Utf16le:
+			return JsonUtf16leDecodeNext(Content, Length, Offset, Character);
+		break;
+
+		default:
+			return 0;
+		break;
+	}
+}
+
+
+size_t JsonUtfEncode(tJsonUtfType UtfType, uint8_t *Content, size_t Size, size_t Offset, tJsonCharacter Character)
+{
+	switch (UtfType)
+	{
+		case json_Utf8:
+			return JsonUtf8Encode(Content, Size, Offset, Character);
+		break;
+
+		case json_Utf16be:
+			return JsonUtf16beEncode(Content, Size, Offset, Character);
+		break;
+
+		case json_Utf16le:
+			return JsonUtf16leEncode(Content, Size, Offset, Character);
+		break;
+
+		default:
+			return 0;
+		break;
+	}
+}
---
+++ source/json/json_utf.h
@@ -0,0 +1,70 @@
+#ifndef JSON_UTF_H
+#define JSON_UTF_H
+
+#include "json_character.h"
+
+
+/**
+ * @brief Enumeration used to define different UTF types
+ */
+typedef enum
+{
+	json_UtfUnknown, /**< Unknown UTF type */
+	json_Utf8,       /**< UTF-8 */
+	json_Utf16be,    /**< UTF-16 (big endian) */
+	json_Utf16le     /**< UTF-16 (little endian) */
+} tJsonUtfType;
+
+
+/**
+ * @brief The maximum number of bytes that an encoded UTF character can have
+ */
+#define JSON_UTF_MAX_SIZE 4
+
+
+/**
+ * @brief Type that can hold a UTF encoded character
+ */
+typedef uint8_t tJsonUtf[JSON_UTF_MAX_SIZE];
+
+
+/**
+ * @brief Determines the type of UTF that is used at the start of a JSON content
+ * @param Content The start of the JSON content
+ * @param Length  The length of the JSON content
+ * @param UtfType Used to return the type of UTF that is being used - \a `json_UtfUnknown` is returned of the type of UTF could not be determined
+ * @return The offset to the start of the actual JSON content (skipping over any 'byte order mark' character that may be present)
+ */
+size_t JsonUtfGetType(const uint8_t *Content, size_t Length, tJsonUtfType *UtfType);
+
+
+/**
+ * @brief Encodes a character in a particular
+ * @param UtfType   The type of UTF to use to encode the character
+ * @param Content   The UTF encoded content
+ * @param Size      The maximum size of UTF encoded content
+ * @param Offset    The offset in the UTF encoded content where the character should be encoded
+ * @param Character The character to encode
+ * @return The length of the encoded character.
+ * @return A zero value is returned if the character could not be encoded
+ * @note This function can only be used to add a character to the end the UTF encoded content.
+ * @note i.e. The current length of \a `Content` should be passed in the parameter \a `Offset`.
+ */
+size_t JsonUtfEncode(tJsonUtfType UtfType, uint8_t *Content, size_t Size, size_t Offset, tJsonCharacter Character);
+
+
+/**
+ * @brief Decodes the next character in a UTF encoded content
+ * @param UtfType   The type of UTF to use to decode the character
+ * @param Content   The UTF encoded content
+ * @param Length    The length of the UTF encoded content
+ * @param Offset    The offset to the start of the next character to decode
+ * @param Character Used to return the next character
+ * @return The decoded length of the next character.
+ * @return A zero value is returned if the next character could not be decoded.
+ * @note Add the returned length to the \a `Offset` value to advance it to the next character in the content.
+ */
+size_t JsonUtfDecode(tJsonUtfType UtfType, const uint8_t *Content, size_t Length, size_t Offset, tJsonCharacter *Character);
+
+
+#endif
